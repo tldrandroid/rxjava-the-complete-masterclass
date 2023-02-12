6e01afa6c4878b1b66b63448a09bc23bee8662de
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2023-02-12 18:39:41 -0500

    Finish Completables, Singles, Maybes

--- app/src/main/java/com/tldrandroid/rxjavamasterclass/model/CompletablesSingleMaybesViewModel.kt
+++ app/src/main/java/com/tldrandroid/rxjavamasterclass/model/CompletablesSingleMaybesViewModel.kt
@@ -7,6 +7,9 @@ import com.tldrandroid.rxjavamasterclass.utils.completablessinglesmaybes.Complet
 import com.tldrandroid.rxjavamasterclass.utils.completablessinglesmaybes.MaybeTask
 import com.tldrandroid.rxjavamasterclass.utils.completablessinglesmaybes.SingleTask
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
@@ -14,7 +17,7 @@ import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-@SuppressLint("CheckResult") // Reason discussed in a later lesson
+@SuppressLint("CheckResult") // Discussed in a later lesson
 class CompletablesSingleMaybesViewModel @Inject constructor(
     private val completableTask: CompletableTask,
     private val maybeTask: MaybeTask,
@@ -50,43 +53,209 @@ class CompletablesSingleMaybesViewModel @Inject constructor(
     // region Lesson Code
 
     private fun completableComplete() {
+//        Completable
+//            .complete()
+//            .subscribe {
+//                viewModelState.markCompletableComplete()
+//            }
 
+        Completable
+            .create { emitter ->
+                completableTask.doTaskAlwaysComplete {
+                    emitter.onComplete()
+                }
+            }
+            .subscribe {
+                viewModelState.markCompletableComplete()
+            }
     }
 
     private fun completableError() {
+//        Completable
+//            .error(GenericThrowable)
+//            .subscribe({
+//            }, { error ->
+//                viewModelState.markCompletableError(error)
+//            })
 
+        Completable
+            .create { emitter ->
+                completableTask.doTaskAlwaysError { error ->
+                    emitter.onError(error)
+                }
+            }
+            .subscribe({
+                // Nothing to do
+            }, { error ->
+                viewModelState.markCompletableError(error)
+            })
     }
 
     private fun completableSometimesComplete() {
-
+        Completable
+            .create { emitter ->
+                completableTask.doTaskSometimesComplete(
+                    onComplete = { emitter.onComplete() },
+                    onError = { error -> emitter.onError(error) }
+                )
+            }
+            .subscribe({
+                viewModelState.markCompletableSometimesComplete("Completed this time!")
+            }, {
+                viewModelState.markCompletableSometimesComplete("Did not complete this time :(")
+            })
     }
 
     private fun singleSuccess() {
+//        Single
+//            .just("I finished :)")
+//            .subscribe { outcome ->
+//                viewModelState.markSingleSuccess(outcome)
+//            }
 
+        Single
+            .create { emitter ->
+                singleTask.doTaskAlwaysSuccess { outcome ->
+                    emitter.onSuccess(outcome)
+                }
+            }
+            .subscribe({ outcome ->
+                viewModelState.markSingleSuccess(outcome)
+            }, {
+
+            })
     }
 
     private fun singleError() {
+//        Single
+//            .error<String>(GenericThrowable)
+//            .subscribe({
+//                // Nothing to do
+//            }, { error ->
+//                viewModelState.markSingleError(error)
+//            })
 
+        Single
+            .create<String> { emitter ->
+                singleTask.doTaskAlwaysError { outcome ->
+                    emitter.onError(outcome)
+                }
+            }
+            .subscribe({
+                // Nothing to do
+            }, { error ->
+                viewModelState.markSingleError(error)
+            })
     }
 
     private fun singleSometimesSuccess() {
-
+        Single
+            .create { emitter ->
+                singleTask.doTaskSometimesSuccess(
+                    onSuccess = { outcome -> emitter.onSuccess(outcome) },
+                    onError = { error -> emitter.onError(error) }
+                )
+            }
+            .subscribe({ outcome ->
+                viewModelState.markSingleSometimesSuccess("Completed this time with: $outcome!")
+            }, {
+                viewModelState.markSingleSometimesSuccess("Did not complete this time :(")
+            })
     }
 
     private fun maybeSuccess() {
+//        Maybe
+//            .just("I definitely finished :)")
+//            .subscribe { outcome ->
+//                viewModelState.markMaybeSuccess(outcome)
+//            }
 
+        Maybe
+            .create { emitter ->
+                maybeTask.doTaskAlwaysSuccess { outcome ->
+                    emitter.onSuccess(outcome)
+                }
+            }
+            .subscribe({ outcome ->
+                viewModelState.markMaybeSuccess(outcome)
+            }, {
+
+            })
     }
 
     private fun maybeEmpty() {
+//        Maybe
+//            .empty<String>()
+//            .subscribe({
+//                // Nothing to do
+//            }, {
+//                // Nothing to do
+//            }, {
+//                viewModelState.markMaybeEmpty()
+//            })
 
+        Maybe
+            .create { emitter ->
+                maybeTask.doTaskAlwaysSuccessWithNull { outcome ->
+                    if (outcome == null) {
+                        emitter.onComplete()
+                    } else {
+                        emitter.onSuccess(outcome) // Never happens, for demonstration only
+                    }
+                }
+            }
+            .subscribe({
+                // Nothing to do
+            }, {
+                // Nothing to do
+            }, {
+                viewModelState.markMaybeEmpty()
+            })
     }
 
     private fun maybeError() {
+//        Maybe
+//            .error<String>(GenericThrowable)
+//            .subscribe({
+//                // Nothing to do
+//            }, { error ->
+//                viewModelState.markMaybeError(error)
+//            })
 
+        Maybe
+            .create<String> { emitter ->
+                maybeTask.doTaskAlwaysError { outcome ->
+                    emitter.onError(outcome)
+                }
+            }
+            .subscribe({
+                // Nothing to do
+            }, { error ->
+                viewModelState.markMaybeError(error)
+            })
     }
 
     private fun maybeSometimesSuccess() {
-
+        Maybe
+            .create { emitter ->
+                maybeTask.doTaskSometimesSuccess(
+                    onSuccess = { outcome ->
+                        if (outcome == null) {
+                            emitter.onComplete()
+                        } else {
+                            emitter.onSuccess(outcome)
+                        }
+                    },
+                    onError = { error -> emitter.onError(error) }
+                )
+            }
+            .subscribe({ outcome ->
+                viewModelState.markMaybeSometimesSuccess("Completed this time with: $outcome")
+            }, {
+                viewModelState.markMaybeSometimesSuccess("Did not complete this time :(")
+            }, {
+                viewModelState.markMaybeSometimesSuccess("Completed this time without data!")
+            })
     }
 
     // endregion
